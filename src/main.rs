114177bca217463cf128fd8cd3e2efcb412d098f
Karim DRIDI <karim.dridi@gmail.com>
2021-04-07 17:03:39 +0200

    implement echo command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::path::Path;
+
+fn execute_echo(iter: env::Args) {
+    for (index, argument) in iter.enumerate() {
+        if index > 0 {
+            print!(" ");
+        }
+        print!("{}", argument);
+    }
+    println!("");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    match args.next() {
+        None => {}
+        Some(arg) => match Path::new(&arg).file_name() {
+            None => {}
+            Some(fname) => match fname.to_str() {
+                None => {}
+                Some(fname) => {
+                    if fname == "echo" {
+                        execute_echo(args)
+                    }
+                }
+            },
+        },
+    }
 }
